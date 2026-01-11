4647688ba57ed81da472b25be5fc93ca59a8d134
R3iYaj <rei.huabci.yang@gmail.com>
2026-01-10 22:05:32 -0600

    feature: added stopwatch controller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/StopWatch.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.Utilities;
+
+public class StopWatch {
+
+    private long _startTime;
+
+    public void StartTimer() {
+        _startTime = System.currentTimeMillis();
+    }
+
+    public long GetElapseTime() {
+        return System.currentTimeMillis() - _startTime;
+    }
+
+    public long StopTimer() {
+        return GetElapseTime();
+    }
+
+    public void ResetTimer() {
+        _startTime = 0;
+    }
+}
