c8d209fd01a2197b953b58f559f18fa584ac7006
Chee Yang <chee.yee.yang@gmail.com>
2026-01-10 18:46:06 -0600

    feature: added configured constants and tuning classes from quick start

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeChallenge/PedroPathing/Constants.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.DecodeChallenge.PedroPathing;
+
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.ThreeWheelConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .useSecondaryTranslationalPIDF(true)
+            .useSecondaryHeadingPIDF(true)
+            .useSecondaryDrivePIDF(true)
+            .forwardZeroPowerAcceleration(-36.2007)
+            .lateralZeroPowerAcceleration(-96.5001)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.07, 0, 0.01, 0.025))
+            .headingPIDFCoefficients(new PIDFCoefficients(0.85, 0, 0.02, 0.03))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.6, 0, 0.0001, 0.6, 0.025))
+            .centripetalScaling(0.0005)
+            .mass(10);
+
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.99,
+            100,
+            1,
+            1);
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .threeWheelLocalizer(localizerConstants)
+                .build();
+    }
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .xVelocity(82.6200)
+            .yVelocity(62.2488)
+            .leftFrontMotorName("FrontLeft")
+            .rightFrontMotorName("FrontRight")
+            .leftRearMotorName("BackLeft")
+            .rightRearMotorName("BackRight")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+
+    public static ThreeWheelConstants localizerConstants = new ThreeWheelConstants()
+            .forwardTicksToInches(0.002993)
+            .strafeTicksToInches(0.0029976)
+            .turnTicksToInches(0.03050)
+            .leftPodY(7)
+            .rightPodY(-7)
+            .strafePodX(-2.5)
+            .leftEncoder_HardwareMapName("YLeftEncoder")
+            .rightEncoder_HardwareMapName("YRightEncoder")
+            .strafeEncoder_HardwareMapName("XEncoder")
+            .leftEncoderDirection(Encoder.FORWARD)
+            .rightEncoderDirection(Encoder.REVERSE)
+            .strafeEncoderDirection(Encoder.REVERSE);
+}
