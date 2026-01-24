1f8132e48346bb0f5caf72d7a66ab22854d2e65d
R3iYaj <rei.huabci.yang@gmail.com>
2026-01-24 10:38:31 -0600

    feat: test april tag reader still

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeChallenge/Controllers/AprilTagWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeChallenge/Controllers/AprilTagWebcam.java
@@ -9,6 +9,7 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Decode.HardwareClass;
+import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.RobotMapping;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -19,7 +20,6 @@ import java.util.List;
 public class AprilTagWebcam {
     private AprilTagProcessor _aprilTagProcessor;
     private VisionPortal _visionPortal;
-
     private List<AprilTagDetection> _detectionTags = new ArrayList<>();
 
     private Telemetry _telemetry;
