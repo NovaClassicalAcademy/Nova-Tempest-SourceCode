f2ff108da338d993c5e1275e0690cb32ea978fbe
R3iYaj <rei.huabci.yang@gmail.com>
2026-02-06 00:50:53 -0600

    feat: wip

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeChallenge/OpModes/AutoTestMK1.java
@@ -0,0 +1,95 @@
+package org.firstinspires.ftc.teamcode.DecodeChallenge.OpModes;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.DecodeChallenge.PedroPathing.Constants;
+
+
+@Autonomous (name="AutonMK1", group="Test")
+public class AutoTestMK1 extends OpMode {
+    private Follower _follower;
+    private Timer _pathTimer, _opModeTimer;
+
+    public enum PathState {
+        STARTPOS_SHOOTPOS,
+        SHOOT_AND_PRELOAD;
+
+    }
+
+    PathState _pathstate;
+
+    private final Pose startPositon = new Pose(59.27738264580369, 60.375533428165, Math.toRadians(90));
+    private final Pose shootPosition = new Pose(59.27738264580369, 60.375533428165, Math.toRadians(90));
+
+    private PathChain driveStatePosShootPos;
+
+
+    public void BuildPaths(){
+        driveStatePosShootPos = _follower.pathBuilder()
+                .addPath(new BezierLine(startPositon, shootPosition))
+                .setLinearHeadingInterpolation(startPositon.getHeading(), shootPosition.getHeading())
+                .build();
+    }
+
+
+    public void statePathUpdate(){
+        switch (_pathstate){
+            case STARTPOS_SHOOTPOS:
+                _follower.followPath(driveStatePosShootPos, true);
+                setPathState(PathState.SHOOT_AND_PRELOAD);
+                break;
+
+            case SHOOT_AND_PRELOAD:
+                if(!_follower.isBusy()){
+                    telemetry.addLine("Done with Path 1");
+                }
+                break;
+
+            default:
+                telemetry.addLine("No State Commanded");
+                break;
+        }
+
+    }
+
+    public void setPathState(PathState newState){
+        _pathstate = newState;
+        _pathTimer.resetTimer();
+    }
+
+    @Override
+    public void init() {
+        _pathstate = PathState.STARTPOS_SHOOTPOS;
+        _follower = Constants.createFollower(hardwareMap);
+        _pathTimer = new Timer();
+        _opModeTimer = new Timer();
+        _opModeTimer.resetTimer();
+
+        BuildPaths();
+        _follower.setPose(startPositon);
+    }
+
+    public void start(){
+        _opModeTimer.resetTimer();
+        setPathState(_pathstate);
+    }
+
+    @Override
+    public void loop() {
+        _follower.update();
+        statePathUpdate();
+
+        telemetry.addData("path state: ", _pathstate.toString());
+        telemetry.addData("x: ", _follower.getPose().getX());
+        telemetry.addData("y: ", _follower.getPose().getY());
+        telemetry.addData("heading: ", _follower.getPose().getHeading());
+        telemetry.addData("path time: ", _pathTimer.getElapsedTimeSeconds());
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeChallenge/OpModes/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeChallenge/OpModes/AutonomousTest.java
@@ -1,125 +1,125 @@
-package org.firstinspires.ftc.teamcode.DecodeChallenge.OpModes;
-
-import com.pedropathing.follower.Follower;
-import com.pedropathing.geometry.BezierLine;
-import com.pedropathing.geometry.Pose;
-import com.pedropathing.paths.PathChain;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.DecodeChallenge.AllianceColor;
-import org.firstinspires.ftc.teamcode.DecodeChallenge.Controllers.IntakeController;
-import org.firstinspires.ftc.teamcode.DecodeChallenge.Paths.BluePathLibrary;
-//import org.firstinspires.ftc.teamcode.DecodeChallenge.Paths.RedPathLibrary;
-//import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.DecodeDriveSystemStateMachine;
-import org.firstinspires.ftc.teamcode.DecodeChallenge.Paths.TestPath;
-import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.RobotMapping;
-import org.firstinspires.ftc.teamcode.DecodeChallenge.PedroPathing.Constants;
-import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.FireSequenceSystemStateMachine;
-
-@Autonomous(name="Autonomous Comp Test", group="Test")
-public class AutonomousTest extends LinearOpMode {
-    private enum RobotState {Start, Preloaded, Return, MoveOffWall, MoveOffLaunch, Idle}
-
-    private RobotState _currentAutoState = RobotState.Start;
-    private RobotMapping _robotMapping;
-//    private DecodeDriveSystemStateMachine _pathing;
-    private FireSequenceSystemStateMachine _fireSequence;
-    private IntakeController _intake;
-    private TestPath _pathTest;
-    private Follower _follower;
-    private ElapsedTime _stateTimer;
-    private AllianceColor _allianceColor = AllianceColor.Blue;
-
-    @Override
-    public void runOpMode() {
-
-        _robotMapping = new RobotMapping(hardwareMap);
-        _follower = Constants.createFollower(hardwareMap);
-
-//        _pathing = new DecodeDriveSystemStateMachine(telemetry, _follower, _robotMapping, _allianceColor);
-//        _pathing.Init();
-
-        _pathTest = new TestPath(_follower);
-
-        _stateTimer = new ElapsedTime();
-
-        _intake = new IntakeController(_robotMapping.UpperLeftIntake, _robotMapping.UpperRightIntake, _robotMapping.LowerLeftIntake, _robotMapping.LowerRightIntake);
-//        _fireSequence = new FireSequenceSystemStateMachine(telemetry, _robotMapping);
-
-        telemetry.addData("OpMode", "Autonomous Comp Test 14");
-        telemetry.update();
-
-        waitForStart();
-
-        // NOTE: initialize launch variable and spin up the launcher.
-//        FireSequenceSystemStateMachine.LaunchState launchState;
-
-        while (opModeIsActive()) {
-            _follower.update();
-
-            switch (_currentAutoState){
-
-                case Start:
-                    if (!_follower.isBusy()) {
-                        _follower.followPath(_pathTest.Path1);
-                        _currentAutoState = RobotState.Preloaded;
-                        _stateTimer.reset();
-                    }
-                    break;
-
-                case Preloaded:
-                    if (!_follower.isBusy() && _stateTimer.seconds() > 10) {
-                        _follower.followPath(_pathTest.Path2);
-                        _currentAutoState = RobotState.Idle;
-                        _stateTimer.reset();
-                    }
-                    break;
+//package org.firstinspires.ftc.teamcode.DecodeChallenge.OpModes;
 //
-//                case Return:
-//                    if (!_follower.isBusy() && _stateTimer.seconds() > 10) {
-//                        _follower.followPath(_pathTest.);
-//                        _currentAutoState = RobotState.Idle;
-//                    }
-//                    break;
-
-                case Idle:
-                    break;
-            }
-
-            telemetry.addData("x", _follower.getPose().getX());
-            telemetry.addData("y", _follower.getPose().getY());
-            telemetry.addData("heading", _follower.getPose().getHeading());
-            telemetry.addData("isBusy", _follower.isBusy());
-            telemetry.update();
+//import com.pedropathing.follower.Follower;
+//import com.pedropathing.geometry.BezierLine;
+//import com.pedropathing.geometry.Pose;
+//import com.pedropathing.paths.PathChain;
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.util.ElapsedTime;
 //
+//import org.firstinspires.ftc.teamcode.DecodeChallenge.AllianceColor;
+//import org.firstinspires.ftc.teamcode.DecodeChallenge.Controllers.IntakeController;
+//import org.firstinspires.ftc.teamcode.DecodeChallenge.Paths.BluePathLibrary;
+////import org.firstinspires.ftc.teamcode.DecodeChallenge.Paths.RedPathLibrary;
+////import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.DecodeDriveSystemStateMachine;
+//import org.firstinspires.ftc.teamcode.DecodeChallenge.Paths.TestPath;
+//import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.RobotMapping;
+//import org.firstinspires.ftc.teamcode.DecodeChallenge.PedroPathing.Constants;
+//import org.firstinspires.ftc.teamcode.DecodeChallenge.Systems.FireSequenceSystemStateMachine;
+//
+//@Autonomous(name="Autonomous Comp Test", group="Test")
+//public class AutonomousTest extends LinearOpMode {
+//    private enum RobotState {Start, Preloaded, Return, MoveOffWall, MoveOffLaunch, Idle}
+//
+//    private RobotState _currentAutoState = RobotState.Start;
+//    private RobotMapping _robotMapping;
+////    private DecodeDriveSystemStateMachine _pathing;
+//    private FireSequenceSystemStateMachine _fireSequence;
+//    private IntakeController _intake;
+//    private TestPath _pathTest;
+//    private Follower _follower;
+//    private ElapsedTime _stateTimer;
+//    private AllianceColor _allianceColor = AllianceColor.Blue;
+//
+//    @Override
+//    public void runOpMode() {
+//
+//        _robotMapping = new RobotMapping(hardwareMap);
+//        _follower = Constants.createFollower(hardwareMap);
+//
+////        _pathing = new DecodeDriveSystemStateMachine(telemetry, _follower, _robotMapping, _allianceColor);
+////        _pathing.Init();
+//
+//        _pathTest = new TestPath(_follower);
+//
+//        _stateTimer = new ElapsedTime();
+//
+//        _intake = new IntakeController(_robotMapping.UpperLeftIntake, _robotMapping.UpperRightIntake, _robotMapping.LowerLeftIntake, _robotMapping.LowerRightIntake);
+////        _fireSequence = new FireSequenceSystemStateMachine(telemetry, _robotMapping);
+//
+//        telemetry.addData("OpMode", "Autonomous Comp Test 14");
+//        telemetry.update();
+//
+//        waitForStart();
+//
+//        // NOTE: initialize launch variable and spin up the launcher.
+////        FireSequenceSystemStateMachine.LaunchState launchState;
+//
+//        while (opModeIsActive()) {
 //            _follower.update();
 //
-//            switch (_currentAutoState) {
-//                case Start:
-//                    if (step == 1) {
-//                        if (!_follower.isBusy()) {
-//                            _pathing.SetRowDestination(1);
-//                            step = 2;
-//                            _stateTimer.reset();
+//            switch (_currentAutoState){
 //
-//                            telemetry.addData("In Step", "1");
-//                        }
+//                case Start:
+//                    if (!_follower.isBusy()) {
+//                        _follower.followPath(_pathTest.Path1);
+//                        _currentAutoState = RobotState.Preloaded;
+//                        _stateTimer.reset();
 //                    }
 //                    break;
 //
+//                case Preloaded:
+//                    if (!_follower.isBusy() && _stateTimer.seconds() > 10) {
+//                        _follower.followPath(_pathTest.Path2);
+//                        _currentAutoState = RobotState.Idle;
+//                        _stateTimer.reset();
+//                    }
+//                    break;
+////
+////                case Return:
+////                    if (!_follower.isBusy() && _stateTimer.seconds() > 10) {
+////                        _follower.followPath(_pathTest.);
+////                        _currentAutoState = RobotState.Idle;
+////                    }
+////                    break;
+//
 //                case Idle:
-//                    telemetry.addData("In Step", "Finish");
-//                    _follower.holdPoint(_follower.getPose());
-//                    telemetry.addData("Status", "Holding Position");
 //                    break;
 //            }
 //
+//            telemetry.addData("x", _follower.getPose().getX());
+//            telemetry.addData("y", _follower.getPose().getY());
+//            telemetry.addData("heading", _follower.getPose().getHeading());
+//            telemetry.addData("isBusy", _follower.isBusy());
 //            telemetry.update();
-//
+////
+////            _follower.update();
+////
+////            switch (_currentAutoState) {
+////                case Start:
+////                    if (step == 1) {
+////                        if (!_follower.isBusy()) {
+////                            _pathing.SetRowDestination(1);
+////                            step = 2;
+////                            _stateTimer.reset();
+////
+////                            telemetry.addData("In Step", "1");
+////                        }
+////                    }
+////                    break;
+////
+////                case Idle:
+////                    telemetry.addData("In Step", "Finish");
+////                    _follower.holdPoint(_follower.getPose());
+////                    telemetry.addData("Status", "Holding Position");
+////                    break;
+////            }
+////
+////            telemetry.update();
+////
+////        }
+////    }
 //        }
 //    }
-        }
-    }
-}
+//}
